caf0e94263bc71f5f04ce9ae541f7fcf8e3a5f10
MrFutureBoss <146051100+MrFutureBoss@users.noreply.github.com>
2024-07-17 10:47:13 +0700

    commit

--- client/src/layouts/user/students/GroupMember.js
+++ client/src/layouts/user/students/GroupMember.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import React, { useEffect } from "react";
 import { Box, List, ListItem, ListItemText, Avatar } from "@mui/material";
 import { useSelector, useDispatch } from "react-redux";
